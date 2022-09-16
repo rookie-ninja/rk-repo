284b6e8e5625f010c2e926d64e05667912d61eae
dongxuny <50768414+dongxuny@users.noreply.github.com>
2022-09-16 14:49:21 +0800

    Add jaeger middleware for asynq

---
+++ asynq/jaeger_mid.go
@@ -0,0 +1,97 @@
+package rkasynq
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/hibiken/asynq"
+	"github.com/rookie-ninja/rk-entry/v2/middleware/tracing"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/trace"
+	"gopkg.in/yaml.v3"
+	"net/http"
+)
+
+var (
+	noopTracerProvider = trace.NewNoopTracerProvider()
+)
+
+const (
+	spanKey    = "SpanKey"
+	traceIdKey = "traceIdKey"
+)
+
+type basePayload struct {
+	TraceHeader http.Header `json:"-"`
+}
+
+type jaegerConfig struct {
+	Asynq struct {
+		Jaeger struct {
+			rkmidtrace.BootConfig
+		} `yaml:"jaeger"`
+	} `yaml:"asynq"`
+}
+
+func NewJaegerMid(jaegerRaw []byte) (asynq.MiddlewareFunc, error) {
+	conf := &jaegerConfig{}
+	err := yaml.Unmarshal(jaegerRaw, conf)
+
+	if err != nil {
+		return nil, err
+	}
+
+	mid := &TraceMiddleware{
+		set: rkmidtrace.NewOptionSet(rkmidtrace.ToOptions(&conf.Asynq.Jaeger.BootConfig, "worker", "ansynq")...),
+	}
+
+	return mid.Middleware, nil
+}
+
+type TraceMiddleware struct {
+	set rkmidtrace.OptionSetInterface
+}
+
+func (m *TraceMiddleware) Middleware(h asynq.Handler) asynq.Handler {
+	return asynq.HandlerFunc(func(ctx context.Context, t *asynq.Task) error {
+		var p basePayload
+		if err := json.Unmarshal(t.Payload(), &p); err != nil {
+			return fmt.Errorf("json.Unmarshal failed: %v: %w", err, asynq.SkipRetry)
+		}
+
+		ctx = m.set.GetPropagator().Extract(ctx, propagation.HeaderCarrier(p.TraceHeader))
+
+		// create new span
+		ctx, span := m.set.GetTracer().Start(ctx, t.Type())
+		defer span.End()
+
+		ctx = context.WithValue(ctx, spanKey, span)
+		ctx = context.WithValue(ctx, traceIdKey, span.SpanContext().TraceID())
+
+		err := h.ProcessTask(ctx, t)
+
+		if err != nil {
+			span.SetStatus(codes.Error, fmt.Sprintf("%v", err))
+		} else {
+			span.SetStatus(codes.Ok, "success")
+		}
+
+		return err
+	})
+}
+
+func GetSpan(ctx context.Context) trace.Span {
+	if v := ctx.Value(spanKey); v != nil {
+		if res, ok := v.(trace.Span); ok {
+			return res
+		}
+	}
+
+	_, span := noopTracerProvider.Tracer("rk-trace-noop").Start(ctx, "noop-span")
+	return span
+}
+
+func GetTraceId(ctx context.Context) string {
+	return GetSpan(ctx).SpanContext().TraceID().String()
+}
